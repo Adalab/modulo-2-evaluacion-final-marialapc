de398fd2acfaf510a3151d7f4071d88c6b22d795
marialapc <marialopez.apc@gmail.com>
2022-11-11 19:39:11 +0100

    create functions to refactor the code

--- src/js/main.js
+++ src/js/main.js
@@ -1,28 +1,31 @@
+/* eslint-disable quotes */
+
 "use strict";
 
 const listCharacters = document.querySelector(".js-characters-list");
 let characters = [];
 
+function renderCharacters() {
+  let html = "";
 
-fetch("https://breakingbadapi.com/api/characters"
-)
-  .then((response) => response.json())
-  .then((data) => {
-    characters = data;
-    console.log(characters);
+  for (const oneCharacter of characters) {
+    html += `<li class="item"><article class="character">`;
+    html += `<img class="character__image"src="${oneCharacter.img}">`;
+    html += `<h2 class="character__name">${oneCharacter.name}</h2>`;
+    html += `<p class="character__dead">${oneCharacter.status}</p>`;
+    html += `</article> </li>`;
+  }
+  listCharacters.innerHTML = html;
+}
 
-    let html = "";
-    
-    for ( const oneCharacter of characters) {
-    
-        html += `<li class="item"><article class="character">`;
-        html += `<img class="character__image"src="${oneCharacter.img}">`;
-        html += `<h2 class="character__name">${oneCharacter.name}</h2>`;
-        html += `<p class="character__dead">${oneCharacter.status}</p>`;
-        html += `</article> </li>`;
-    }
-    listCharacters.innerHTML = html;
+function getData() {
+  fetch("https://breakingbadapi.com/api/characters")
+    .then((response) => response.json())
+    .then((data) => {
+      characters = data;
+      console.log(characters);
+      renderCharacters();
+    });
+}
 
-   });
-
-
+getData();

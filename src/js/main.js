725375cfb5203ea04a3ffa0123516733cfab1630
marialapc <marialopez.apc@gmail.com>
2022-11-11 19:25:55 +0100

    paint characters in the html

--- src/js/main.js
+++ src/js/main.js
@@ -9,6 +9,20 @@ fetch("https://breakingbadapi.com/api/characters"
   .then((response) => response.json())
   .then((data) => {
     characters = data;
+    console.log(characters);
+
+    let html = "";
+    
+    for ( const oneCharacter of characters) {
+    
+        html += `<li class="item"><article class="character">`;
+        html += `<img class="character__image"src="${oneCharacter.img}">`;
+        html += `<h2 class="character__name">${oneCharacter.name}</h2>`;
+        html += `<p class="character__dead">${oneCharacter.status}</p>`;
+        html += `</article> </li>`;
+    }
+    listCharacters.innerHTML = html;
+
    });
 
 

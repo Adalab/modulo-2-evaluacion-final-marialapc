6f5d9b0fa7858edf341cfdef6c882d1938d732e7
marialapc <marialopez.apc@gmail.com>
2022-11-11 19:01:56 +0100

    fetch to the Api

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,14 @@
-'use strict';
+"use strict";
+
+const listCharacters = document.querySelector(".js-characters-list");
+let characters = [];
+
+
+fetch("https://breakingbadapi.com/api/characters"
+)
+  .then((response) => response.json())
+  .then((data) => {
+    characters = data;
+   });
 
 
